5d7aee741a7c9145e4752ae479f498f152f40b1b
Elaine Ulsh <112116883+ElaineUlsh@users.noreply.github.com>
2023-03-01 13:43:34 -0500

    Start recipe class to parse recipe json.

---
+++ lib/recipe.dart
@@ -0,0 +1,20 @@
+//makes recipe objects from json information
+//takes json
+//stores the keys as variables
+//decode method
+//encode method
+import 'dart:convert';
+
+class Recipe {
+  late final jsonData;
+  String string;
+
+  Recipe(this.jsonData) {
+    jsonDecoder();
+  }
+
+  String jsonDecoder() {
+    string = jsonData.decode();
+    return string;
+  }
+}
