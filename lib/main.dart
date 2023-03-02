933590b5a48be9cc5f933db6f4e2f0172c8f1f82
Ryan Martinez <51921174+ryanmartinez24@users.noreply.github.com>
2023-03-01 21:35:43 -0500

    Attempt to allow switching between pages.
    Did not work. Need to talk to Dr. G about why

--- lib/main.dart
+++ lib/main.dart
@@ -19,18 +19,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class recipeWidget extends StatefulWidget {
+  const recipeWidget({super.key});
+
+  @override
+  State<recipeWidget> createState() => _recipeWidgetState();
+}
+
+class _recipeWidgetState extends State<recipeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [ElevatedButton(onPressed: goHome, child: Text("Go Back"))],
+        ),
+      ),
+    );
+  }
+
+  void goHome() {
+    Navigator.pop(context);
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -41,14 +55,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     List<DropdownMenuItem<String>> strings = [
-      const DropdownMenuItem(
-          value: "Orange Chicken", child: Text("Orange Chicken")),
+      DropdownMenuItem(
+        value: "Orange Chicken",
+        child: Text("Orange Chicken"),
+        onTap: onTapOrangeChicken,
+      ),
       const DropdownMenuItem(
           value: "Oreo Dirt Pie", child: Text("Oreo Dirt Pie")),
       const DropdownMenuItem(
         value: "Caesar Salad",
         child: Text("Caesar Salad,"),
-      )
+      ),
     ];
     return Scaffold(
         body: Center(
@@ -58,6 +75,13 @@ class _MyHomePageState extends State<MyHomePage> {
           const Text("Cookbook!"),
           DropdownButton(items: strings, onChanged: onChanged)
         ])));
+  }
+
+  void onTapOrangeChicken() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => recipeWidget()),
+    );
   }
 
   void onChanged(String? s) {
