674148970f03c0c0aae5369f395905ecc1308cca
Ryan Martinez <ryan.martinez@bsu.edu>
2023-03-03 13:53:38 -0500

    Restructure recipe class and fix recipe tests accordingly

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -3,34 +3,27 @@
 //stores the keys as variables
 //decode method
 //encode method
-import 'dart:convert';
 import 'package:fp_recipe_book/ingredient.dart';
 
 class Recipe {
-  late final jsonData;
+  final String directions;
+  final String description;
+  final List<Ingredient> ingredients;
+
   late final decodedJsonObject;
 
-  Recipe(this.jsonData) {
-    jsonDecoder();
-  }
-
-  String jsonDecoder() {
-    decodedJsonObject = jsonData.decode();
-    return decodedJsonObject;
-  }
+  Recipe(this.directions, this.description, this.ingredients);
 
   List<Ingredient> ingredientRetriever() {
-    int numOfIngredients =
-        decodedJsonObject["Recipes"]["recipe1"]["Ingredients"].length;
+    int numOfIngredients = decodedJsonObject["recipe1"]["Ingredients"].length;
     List<Ingredient> ingredients = [];
     for (int i = 0; i < numOfIngredients; i++) {
-      String name = decodedJsonObject["Recipes"]["recipe1"]["Ingredients"]
-          .keys
-          .elementAt(i);
-      int amount = int.parse(
-          (decodedJsonObject["Recipes"]["recipe1"]["Ingredients"][name][0]));
+      String name =
+          decodedJsonObject["recipe1"]["Ingredients"].keys.elementAt(i);
+      int amount =
+          int.parse((decodedJsonObject["recipe1"]["Ingredients"][name][0]));
       String measurementUnit =
-          decodedJsonObject["Recipes"]["recipe1"]["Ingredients"][name][1];
+          decodedJsonObject["recipe1"]["Ingredients"][name][1];
 
       Ingredient test = Ingredient(name, measurementUnit, amount);
       ingredients.add(test);
