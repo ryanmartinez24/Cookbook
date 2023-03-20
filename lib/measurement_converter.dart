9c8e0d2132cc7e3f1f9950cb8a49a88c5a71e39c
JalenH <jmhart3@bsu.edu>
2023-03-19 22:51:48 -0400

    Unit conversion class now will do both scale the ingredient up or down to make the unit the most legible option for both application and common sense

--- lib/measurement_converter.dart
+++ lib/measurement_converter.dart
@@ -21,6 +21,8 @@ class MeasurementConverter {
 
   unitConverter(double n, String unit) {
     if (unit == "tsp") {
+      if(n<3){
+      }
       if (n >= 3 && n < 6) {
         unit = "tbsp";
         n = (n / 3);
@@ -48,6 +50,12 @@ class MeasurementConverter {
       amount = (n * 1000).round().toDouble() / 1000;
     }
     if (unit == "tbsp") {
+      if (n<1){
+        unit = "tsp";
+        n = (n*3);
+      }
+      if (n>= 1 && n<2){
+      }
       if (n >= 2 && n < 16) {
         unit = "oz";
         n = (n / 6);
@@ -71,6 +79,16 @@ class MeasurementConverter {
       amount = (n * 1000).round().toDouble() / 1000;
     }
     if (unit == "oz") {
+      if (n >= 0 && n<0.5){
+        unit = "tsp";
+        n = (n*6);
+      }
+      if (n >= 0.5  && n<1){
+        unit = "tbsp";
+        n = (n*2);
+      }
+      if (n >= 1 && n <8){
+      }
       if (n >= 8 && n < 16) {
         unit = "cup";
         n = (n / 8);
@@ -90,6 +108,20 @@ class MeasurementConverter {
       amount = (n * 1000).round().toDouble() / 1000;
     }
     if (unit == "cup") {
+      if(n >= 0 && n< 0.0625){
+        n = (n*48);
+        unit = "tsp";
+      }
+      if(n >= 0.0625 && n< 0.125){
+        n = (n*16);
+        unit = "tbsp";
+      }
+      if(n >= 0.125 && n<1){
+        n = (n*8);
+        unit = "oz";
+      }
+      if(n >= 1 && n<2){
+      }
       if (n >= 2 && n < 4) {
         unit = "pint";
         n = (n / 2);
@@ -105,6 +137,24 @@ class MeasurementConverter {
       amount = (n * 1000).round().toDouble() / 1000;
     }
     if (unit == "pint") {
+      if(n >= 0 && n<0.0313){
+        n = (n*96);
+        unit = "tsp";
+      }
+      if(n >= 0.0313 && n<0.0625){
+        n = (n*32);
+        unit = "tbsp";
+      }
+      if(n >= 0.0625 && n<0.5){
+        n = (n*16);
+        unit = "oz";
+      }
+      if(n >= 0.5 && n<1){
+        n = (n*2);
+        unit = "cup";
+      }
+      if (n >=1  && n<2) {
+      }
       if (n >= 2 && n < 8) {
         unit = "quart";
         n = (n / 2);
@@ -116,10 +166,57 @@ class MeasurementConverter {
       amount = (n * 1000).round().toDouble() / 1000;
     }
     if (unit == "quart") {
-      unit = "gallon";
-      n = (n / 4);
+      if (n >= 0 && n<0.0104){
+        n = (n*1111111);
+        unit = "tsp";
+      }
+      if(n >= 0.0104 && n<0.0313){
+        n = (n*96);
+        unit = "tbsp";
+      }
+      if(n >= 0.0313 && n<0.0625){
+        n = (n*32);
+        unit = "oz";
+      }
+      if(n >= 0.0625 && n<0.5){
+        n = (n*16);
+        unit = "cup";
+      }
+      if(n >= 0.5 && n<1){
+        n = (n*2);
+        unit = "pint";
+      }
+      if (n >=1  && n<4) {
+      }
+      if(n >= 4) {
+        unit = "gallon";
+        n = (n / 4);
+      }
+      if (unit == "gallon"){
+        if (n >= 0 && n<0.0020){
+          n = (n*1536);
+          unit = "tsp";
+        }
+        if(n >= 0.0020 && n<0.0039){
+          n = (n*512);
+          unit = "tbsp";
+        }
+        if(n >= 0.0039 && n<0.0313){
+          n = (n*256);
+          unit = "oz";
+        }
+        if(n >= 0.0313 && n<0.25){
+          n = (n*32);
+          unit = "cup";
+        }
+        if(n >= 0.25 && n<1){
+          n = (n*4);
+          unit = "quart";
+        }
+        if (n >=1) {
+        }
+      }
     }
-
     amount = (n * 1000).round().toDouble() / 1000;
     this.unit = unit;
   }
