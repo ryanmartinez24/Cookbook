5ffb4ba36595e3d1f118bf2a29b1f54d3721b975
Elaine Ulsh <elaine.ulsh@bsu.edu>
2023-04-02 15:03:35 -0400

    Refactor the measurements class for simplifying down.

--- lib/measurement.dart
+++ lib/measurement.dart
@@ -4,238 +4,47 @@ class Measurement {
 
   List<List> unitTable = [
     ['pinch', 2],
-    ['tsp', 3],
-    ['tbsp', 3],
-    ['1/4 cup', 2],
-    ['1/2 cup', 2],
-    ['cups', 4],
-    ['quarts', 4],
-    ['gallons']
+    ['tsp', 3, 0.125],
+    ['tbsp', 3, 0.5],
+    ['cups', 4, 0.25],
+    ['quarts', 4, 1],
+    ['gallons', 1, 1]
   ];
 
   Measurement(this.unit, this.amount) {
-    _simplify();
-  }
-
-  void _simplify() {
-    for (int i = 0; i < unitTable.length; i++) {
-      if (unit == unitTable[i][0]) {
-        if (amount >= unitTable[i][1]) {
-          unit = unitTable[i + 1][0];
-          amount = amount / unitTable[i][1];
-          _simplify();
-        } else {
-          break;
-        }
-      }
-    }
+    _simplifyUp();
+    _simplifyDown();
   }
 
   void scale(double scale) {
     amount = amount * scale;
-    _simplify();
+
+    if (scale > 1) {
+      _simplifyUp();
+    } else {
+      _simplifyDown();
+    }
   }
 
-  /*
-  void _simplify() {
-    double n = amount;
-    if (unit == "pinches") {
-      if (n > 16) {
-        unit = "tsp";
-        n = n / 16;
-      }
-    }
-    if (unit == "tsp") {
-      if (n >= 3 && n < 6) {
-        unit = "tbsp";
-        n = (n / 3);
-      }
-      if (n >= 6 && n < 48) {
-        unit = "oz";
-        n = (n / 6);
-      }
-      if (n >= 48 && n < 96) {
-        unit = "cup";
-        n = (n / 48);
-      }
-      if (n >= 96 && n < 192) {
-        unit = "pint";
-        n = (n / 96);
-      }
-      if (n >= 192 && n < 768) {
-        unit = "quart";
-        n = (n / 192);
-      }
-      if (n >= 768) {
-        unit = "gallon";
-        n = (n / 768);
-      }
-      amount = (n * 1000).round().toDouble() / 1000;
-    }
-    if (unit == "tbsp") {
-      if (n < 1) {
-        unit = "tsp";
-        n = (n * 3);
-      }
-      if (n >= 1 && n < 2) {}
-      //if (n >= 2 && n < 16) {
-      //  unit = "oz";
-      //   n = (n / 6);
-      // }
-      if (n >= 16 && n < 32) {
-        unit = "cup";
-        n = (n / 16);
-      }
-      if (n >= 32 && n < 64) {
-        unit = "pint";
-        n = (n / 32);
-      }
-      if (n >= 64 && n < 256) {
-        unit = "quart";
-        n = (n / 64);
-      }
-      if (n >= 256) {
-        unit = "gallon";
-        n = (n / 256);
-      }
-      amount = (n * 1000).round().toDouble() / 1000;
-    }
-    if (unit == "oz") {
-      if (n >= 0 && n < 0.5) {
-        unit = "tsp";
-        n = (n * 6);
-      }
-      if (n >= 0.5 && n < 1) {
-        unit = "tbsp";
-        n = (n * 2);
-      }
-      if (n >= 1 && n < 8) {}
-      if (n >= 8 && n < 16) {
-        unit = "cup";
-        n = (n / 8);
-      }
-      if (n >= 16 && n < 32) {
-        unit = "pint";
-        n = (n / 16);
-      }
-      if (n >= 32 && n < 128) {
-        unit = "quart";
-        n = (n / 32);
-      }
-      if (n >= 128) {
-        unit = "gallon";
-        n = (n / 128);
-      }
-      amount = (n * 1000).round().toDouble() / 1000;
-    }
-    if (unit == "cup") {
-      if (n >= 0 && n < 0.0625) {
-        n = (n * 48);
-        unit = "tsp";
-      }
-      if (n >= 0.0625 && n < 0.125) {
-        n = (n * 16);
-        unit = "tbsp";
-      }
-      if (n >= 0.125 && n < 1) {
-        n = (n * 8);
-        unit = "oz";
-      }
-      if (n >= 1 && n < 2) {}
-      if (n >= 2 && n < 4) {
-        unit = "pint";
-        n = (n / 2);
-      }
-      if (n >= 4 && n < 8) {
-        unit = "quart";
-        n = (n / 4);
-      }
-      if (n >= 16) {
-        unit = "gallon";
-        n = (n / 16);
-      }
-      amount = (n * 1000).round().toDouble() / 1000;
-    }
-    if (unit == "pint") {
-      if (n >= 0 && n < 0.0313) {
-        n = (n * 96);
-        unit = "tsp";
-      }
-      if (n >= 0.0313 && n < 0.0625) {
-        n = (n * 32);
-        unit = "tbsp";
-      }
-      if (n >= 0.0625 && n < 0.5) {
-        n = (n * 16);
-        unit = "oz";
-      }
-      if (n >= 0.5 && n < 1) {
-        n = (n * 2);
-        unit = "cup";
-      }
-      if (n >= 1 && n < 2) {}
-      if (n >= 2 && n < 8) {
-        unit = "quart";
-        n = (n / 2);
-      }
-      if (n >= 8) {
-        unit = "gallon";
-        n = (n / 8);
-      }
-      amount = (n * 1000).round().toDouble() / 1000;
-    }
-    if (unit == "quart") {
-      if (n >= 0 && n < 0.0104) {
-        n = (n * 1111111);
-        unit = "tsp";
-      }
-      if (n >= 0.0104 && n < 0.0313) {
-        n = (n * 96);
-        unit = "tbsp";
-      }
-      if (n >= 0.0313 && n < 0.0625) {
-        n = (n * 32);
-        unit = "oz";
-      }
-      if (n >= 0.0625 && n < 0.5) {
-        n = (n * 16);
-        unit = "cup";
-      }
-      if (n >= 0.5 && n < 1) {
-        n = (n * 2);
-        unit = "pint";
-      }
-      if (n >= 1 && n < 4) {}
-      if (n >= 4) {
-        unit = "gallon";
-        n = (n / 4);
-      }
-      if (unit == "gallon") {
-        if (n >= 0 && n < 0.0020) {
-          n = (n * 1536);
-          unit = "tsp";
+  void _simplifyUp() {
+    for (int i = 0; i < unitTable.length - 1; i++) {
+      if (unit == unitTable[i][0]) {
+        while (amount >= unitTable[i][1]) {
+          unit = unitTable[i + 1][0];
+          amount = amount / unitTable[i][1];
         }
-        if (n >= 0.0020 && n < 0.0039) {
-          n = (n * 512);
-          unit = "tbsp";
-        }
-        if (n >= 0.0039 && n < 0.0313) {
-          n = (n * 256);
-          unit = "oz";
-        }
-        if (n >= 0.0313 && n < 0.25) {
-          n = (n * 32);
-          unit = "cup";
-        }
-        if (n >= 0.25 && n < 1) {
-          n = (n * 4);
-          unit = "quart";
-        }
-        if (n >= 1) {}
       }
     }
-    amount = (n * 1000).round().toDouble() / 1000;
   }
 
-   */
+  void _simplifyDown() {
+    for (int i = unitTable.length - 1; i > 0; i--) {
+      if (unit == unitTable[i][0]) {
+        while (amount < unitTable[i][2]) {
+          unit = unitTable[i - 1][0];
+          amount = amount * unitTable[i - 1][1];
+        }
+      }
+    }
+  }
 }
--- test/measurement_test.dart
+++ test/measurement_test.dart
@@ -4,11 +4,13 @@ import 'package:fp_recipe_book/ingredient.dart';
 
 void main() {
   Ingredient i1 = Ingredient("Pepper", Measurement("whole", 3.0));
-  Ingredient i3 = Ingredient("Olive Oil", Measurement("tsp", 3.0));
+  Ingredient i2 = Ingredient("Olive Oil", Measurement("tsp", 3.0));
+  Ingredient i3 = Ingredient('Butter', Measurement('quarts', 2));
+  Ingredient i4 = Ingredient('Butter', Measurement('gallons', 4.0));
 
   test('testing that measurement simplifies on creation', () {
-    expect(i3.measurement.unit, "tbsp");
-    expect(i3.measurement.amount, 1.0);
+    expect(i2.measurement.unit, "tbsp");
+    expect(i2.measurement.amount, 1.0);
   });
 
   test('testing that scale function works', () {
@@ -17,8 +19,21 @@ void main() {
   });
 
   test('testing that scale function simplifies after', () {
-    i3.measurement.scale(3);
-    expect(i3.measurement.unit, 'tbsp');
+    i2.measurement.scale(3);
+    expect(i2.measurement.unit, 'cups');
+    expect(i2.measurement.amount, 1.0);
+  });
+
+  test('I can convert the unit to gallons', () {
+    i3.measurement.scale(6);
+    expect(i3.measurement.unit, 'gallons');
     expect(i3.measurement.amount, 3.0);
+  });
+
+  test('I can convert the measurement to a smaller unit', () {
+    i4.measurement.scale(1 / 8);
+    print('unit: ${i4.measurement.unit}; amount: ${i4.measurement.amount}');
+    expect(i4.measurement.unit, 'quarts');
+    expect(i4.measurement.amount, 2.0);
   });
 }
