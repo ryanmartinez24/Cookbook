16cc3e2876b713d4af4d755c8e9a2abda0b37910
Ryan Martinez <ryan.martinez@bsu.edu>
2023-04-13 14:22:39 -0400

    fix clean code naming issue

--- test/measurement_test.dart
+++ test/measurement_test.dart
@@ -3,36 +3,37 @@ import 'package:test/test.dart';
 import 'package:fp_recipe_book/ingredient.dart';
 
 void main() {
-  Ingredient i1 = Ingredient("Pepper", Measurement("whole", 3.0));
-  Ingredient i2 = Ingredient("Olive Oil", Measurement("tsp", 3.0));
-  Ingredient i3 = Ingredient('Butter', Measurement('quarts', 2));
-  Ingredient i4 = Ingredient('Butter', Measurement('gallons', 4.0));
+  Ingredient pepper = Ingredient("Pepper", Measurement("whole", 3.0));
+  Ingredient oliveOil = Ingredient("Olive Oil", Measurement("tsp", 3.0));
+  Ingredient butterInQuarts = Ingredient('Butter', Measurement('quarts', 2));
+  Ingredient butterInGallons =
+      Ingredient('Butter', Measurement('gallons', 4.0));
 
   test('testing that measurement simplifies on creation', () {
-    expect(i2.measurement.unit, "tbsp");
-    expect(i2.measurement.amount, 1.0);
+    expect(oliveOil.measurement.unit, "tbsp");
+    expect(oliveOil.measurement.amount, 1.0);
   });
 
   test('testing that scale function works', () {
-    i1.measurement.scale(5);
-    expect(i1.measurement.amount, 15);
+    pepper.measurement.scale(5);
+    expect(pepper.measurement.amount, 15);
   });
 
   test('testing that scale function simplifies after', () {
-    i2.measurement.scale(3);
-    expect(i2.measurement.unit, 'cups');
-    expect(i2.measurement.amount, 1.0);
+    oliveOil.measurement.scale(3);
+    expect(oliveOil.measurement.unit, 'cups');
+    expect(oliveOil.measurement.amount, 1.0);
   });
 
   test('I can convert the unit to gallons', () {
-    i3.measurement.scale(6);
-    expect(i3.measurement.unit, 'gallons');
-    expect(i3.measurement.amount, 3.0);
+    butterInQuarts.measurement.scale(6);
+    expect(butterInQuarts.measurement.unit, 'gallons');
+    expect(butterInQuarts.measurement.amount, 3.0);
   });
 
   test('I can convert the measurement to a smaller unit', () {
-    i4.measurement.scale(1 / 8);
-    expect(i4.measurement.unit, 'quarts');
-    expect(i4.measurement.amount, 2.0);
+    butterInGallons.measurement.scale(1 / 8);
+    expect(butterInGallons.measurement.unit, 'quarts');
+    expect(butterInGallons.measurement.amount, 2.0);
   });
 }
